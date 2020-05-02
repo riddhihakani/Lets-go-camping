7ca4b424b1bd915168ad202a43921133d1cb2839
riddhihakani <riddhi1652000h@gmail.com>
2020-05-02 18:45:56 +0530

    added password

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ var commentRoutes    = require("./routes/comments"),
 //mongoose.connect("mongodb+srv://riddhih:<RIDPAL>@yelpcamp-4iml6.mongodb.net/test?retryWrites=true&w=majority",{useNewUrlParser : true, useUnifiedTopology:true});
 
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://riddhih:<password>@yelpcamp-4iml6.mongodb.net/test?retryWrites=true&w=majority";
+const uri = "mongodb+srv://riddhih:<RIDPAL>@yelpcamp-4iml6.mongodb.net/test?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology : true });
 client.connect(err => {
   const collection = client.db("test").collection("devices");
